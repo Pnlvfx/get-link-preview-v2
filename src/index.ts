2c04f3341dd4c87648adf467352c31cbdb471fcd
simone <simonegauli@gmail.com>
2023-07-16 12:33:02 +0200

    first release

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { parser } from 'html-metadata-parser';
+import { APIOutput } from './types';
+
+const getMetadata = async (url: string) => {
+  try {
+    return parser(url);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getLinkPreview = async (url: string): Promise<APIOutput | undefined> => {
+  url = url.toLowerCase();
+  url = url.includes('://') ? url : 'http://' + url;
+
+  const isUrlValid = /[\w#%()+./:=?@~]{2,256}\.[a-z]{2,6}\b([\w#%&+./:=?@~-]*)/gi.test(url);
+
+  if (!url || !isUrlValid) throw new Error('Invalid URL');
+
+  if (url && isUrlValid) {
+    const { hostname } = new URL(url);
+    const metadata = await getMetadata(url);
+    if (!metadata) throw new Error('Cannot find metadata for the given url');
+    const { images, og, meta } = metadata;
+
+    const image = og.image || images ? images?.at(0) : `${process.env.SERVER_URL}/img-placeholder.jpg`;
+    const description = og.description || meta.description || undefined;
+    const title = og.title || meta.title || '';
+    const siteName = og.site_name || '';
+
+    return {
+      title,
+      description,
+      image,
+      siteName,
+      hostname,
+    };
+  }
+};
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export interface APIOutput {
+  title?: string;
+  description?: string;
+  image?: string;
+  siteName?: string;
+  hostname?: string;
+}
